3d32ce68d1084744522be115c6ae0a2f46fe12e4
Silaev Dmitry <dmsilaev@yandex.ru>
2013-07-31 11:28:11 +0200

    First test passed

--- lib/configus.rb
+++ lib/configus.rb
@@ -1,5 +1,19 @@
 require "configus/version"
 
 module Configus
-  # Your code goes here...
+
+  class Builder
+
+    attr_reader :all_env
+
+    def initialize(default_env, &block)
+      @all_env = {}
+      @default_env = default_env
+      instance_eval &block
+    end
+    
+    def env(new_env)
+      @all_env[new_env] = {} 
+    end
+  end
 end
--- test/lib/test_configus.rb
+++ test/lib/test_configus.rb
@@ -1,31 +1,19 @@
 require_relative '../test_helper'
 
 class TestConfigus < MiniTest::Test
-  def test_creation
-    c = Configus.build :development do
+  def setup
+    @builder = Configus::Builder.new :development do
       env :production do
-        website_url 'http://example.com'
-        email do
-          pop do
-            address 'pop.example.com'
-            port    110
-          end
-          smtp do
-            address 'smtp.example.com'
-            port    25
-          end
-        end
+        a "foo"
       end
 
-      env :development, :parent => :production do
-        website_url 'http://text.example.com'
-        email do
-          smtp do
-            address 'smpt.text.example.com'
-          end
-        end
+      env :development do
+        a "bar"
       end
     end
-    assert c
+  end
+
+  def test_env
+    assert @builder.all_env == { :development => {}, :production => {} }
   end
 end

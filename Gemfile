507c25f0aaf97454fb4c181ea4e8f1d8110e5133
Silaev Dmitry <dmsilaev@yandex.ru>
2013-08-01 16:13:35 +0200

    Settings from configus and travic-ci

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 gemspec
 
 gem "minitest"
+gem 'coveralls', require: false
 gem 'guard-minitest'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
-require 'bundler/setup'
-Bundler.require
-require 'minitest/autorun'
-
 if ENV['TRAVIS']
     require 'coveralls'
       Coveralls.wear!
 end
+
+
+require 'bundler/setup'
+Bundler.require
+require 'minitest/autorun'
+
